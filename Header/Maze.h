ac0fe33b046f7522932f8d74d740e23554d052a6
Rohan Antony <rohan271995@gmail.com>
2019-03-19 16:36:50 +0530

    Added algorithm to solve the problem

--- Header/Maze.h
+++ Header/Maze.h
@@ -2,26 +2,50 @@
 #define MAZESOLVER_MAZE_HEADER
 
 #include <iostream>
+#include <queue>
+#include <utility>
 
 using std::cout;
 using std::endl;
+using std::pair;
+
+typedef pair<int, int> Location;
 
 class Maze {
 	int** maze;
 	int rows, cols;
 
-	int* getMazeStart() {
+	Location getMazeStart() {
 		for (int i = 0; i < cols; i++) {
 			if (maze[0][i] == -3)
-				return &maze[0][i];
+				return Location(0, i);
 		}
+		return Location(-1, -1);
 	}
 
-	int* getMazeEnd() {
+	Location getMazeEnd() {
 		for (int i = 0; i < cols; i++) {
 			if (maze[rows - 1][i] == -3)
-				return &maze[0][i];
+				return Location(rows - 1, i);
 		}
+		return Location(-1, -1);
+	}
+
+	int getMaxOfVals(int a, int b, int c, int d) {
+		int max = INT_MAX;
+		if (a < 0 && b < 0 && c < 0 && d < 0)
+			return 0;
+		else {
+			if (a >= 0 && a < max)
+				max = a;
+			if (b >= 0 && b < max)
+				max = b;
+			if (c >= 0 && c < max)
+				max = c;
+			if (d >= 0 && d < max)
+				max = d;			
+		}		
+		return max + 1;
 	}
 
 public:
@@ -36,26 +60,58 @@ public:
 		}
 	}
 
-	int operator()(int row, int col) const{
-		if (row < 0 || row >= rows)
-			return -1; //return a wall
-		else if (col < 0 || col >= cols)
-			return -1;
-		return maze[row][col];
-	}
-
 	int& operator()(int row, int col) {
+		int temp = -1;
+		if (row < 0 || row >= rows)
+			return temp; //return a wall
+		else if (col < 0 || col >= cols)
+			return temp;
 		return maze[row][col];
 	}
 
 	void printCurrentState() {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				cout << maze[i][j] << "\t";
+				if (maze[i][j] == -1)
+					cout << "XXX";
+				else if (maze[i][j] == -2)
+					cout << "   ";
+				else if (maze[i][j] < 0)
+					cout << "< >";
+				else if(maze[i][j] < 10)
+					cout << " " << maze[i][j] << " ";
+				else 
+					cout << maze[i][j] << " ";
 			}
 			cout << endl;
 		}
+		cout << endl << endl;
+	}
 
+	void findShortestPath() {
+		Location start = getMazeStart(), end = getMazeEnd();
+		std::queue<Location> points;
+		points.push(start);
+		while (points.size() != 0) {
+			Location current = points.front();
+			points.pop();
+			int x = current.first, y = current.second;
+			int leftVal = (*this)(x - 1, y),
+				bottomVal = (*this)(x, y + 1),
+				rightVal = (*this)(x + 1, y),
+				topVal = (*this)(x, y - 1);
+			int distance = getMaxOfVals(leftVal, bottomVal, rightVal, topVal);
+			(*this)(x, y) = distance;
+			//printCurrentState();
+			if (leftVal <= -2)
+				points.push(Location(x - 1, y));
+			if (bottomVal <= -2)
+				points.push(Location(x, y + 1));
+			if (rightVal <= -2)
+				points.push(Location(x + 1, y));
+			if (topVal <= -2)
+				points.push(Location(x, y - 1));
+		}
 	}
 
 	~Maze() {
--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # MazeSolver
 
-Application to implement a maze, represent it and solve it with multiple algorithms and check for efficieny.
+- Application to implement a maze, represent it and solve it with multiple algorithms and check for efficieny.
+- Solves the maze problem using a simplistic version of Djikstra with consideration of adjacent nodes having path length 1.
 
 ## Credits
 - For the idea [Maze Solving](https://www.youtube.com/watch?v=rop0W4QDOUI)
 
 ## TODO
-- Add code to implement the algorithms
-
-## Objectives
 - Add a maze generator class
 - Add code to read from image formats with each pixel
-- Add BitArray class to store the maze more efficiently
 - Add SDL support to display the content and also the path
